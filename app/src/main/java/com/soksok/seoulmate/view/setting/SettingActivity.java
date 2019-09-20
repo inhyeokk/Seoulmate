f95a9f23cd563f5cb0d83ba615948e7fee51a072
rkddlsgur983 <rkddlsgur983@naver.com>
2019-09-21 02:52:19 +0900

    feat: Add setting ui sg2

--- app/src/main/java/com/soksok/seoulmate/view/setting/SettingActivity.java
+++ app/src/main/java/com/soksok/seoulmate/view/setting/SettingActivity.java
@@ -6,12 +6,15 @@ import androidx.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.soksok.seoulmate.R;
 import com.soksok.seoulmate.databinding.ActivitySettingBinding;
 
 public class SettingActivity extends AppCompatActivity {
 
     private ActivitySettingBinding binding;
+
+    private BottomSheetBehavior behavior;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,13 +30,19 @@ public class SettingActivity extends AppCompatActivity {
 
     private void setupViews() {
 
+        behavior = BottomSheetBehavior.from(binding.clBottomSheet);
+        setBottomClickable(false);
     }
 
     /*
      * 클릭 이벤트
      */
-    public void onCertifyClick(View v) {
+    public void onLayoutClick(View v) {
+        setBottomClickable(false);
+    }
 
+    public void onCertifyClick(View v) {
+        setBottomClickable(true);
     }
 
     public void onLikeMateClick(View v) {
@@ -50,5 +59,36 @@ public class SettingActivity extends AppCompatActivity {
 
     public void onLogoutClick(View v) {
 
+    }
+
+    public void onBottomCancelClick(View v) {
+        setBottomClickable(false);
+    }
+
+    public void onBottomContactClick(View v) {
+        setBottomClickable(false);
+    }
+
+    private void setBottomClickable(boolean isClickable) {
+
+        if (isClickable) {
+            behavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+            binding.tvBottomCancel.setClickable(true);
+            binding.tvBottomCancel.setFocusable(true);
+            binding.tvBottomContact.setClickable(true);
+            binding.tvBottomContact.setFocusable(true);
+            binding.clBackground.setVisibility(View.VISIBLE);
+            binding.clBackground.setClickable(true);
+            binding.clBackground.setFocusable(true);
+        } else {
+            behavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+            binding.tvBottomCancel.setClickable(false);
+            binding.tvBottomCancel.setFocusable(false);
+            binding.tvBottomContact.setClickable(false);
+            binding.tvBottomContact.setFocusable(false);
+            binding.clBackground.setVisibility(View.GONE);
+            binding.clBackground.setClickable(false);
+            binding.clBackground.setFocusable(false);
+        }
     }
 }
