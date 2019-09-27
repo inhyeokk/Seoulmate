4bb9cf9591d40d942ce22fba74256593f6656f5a
rkddlsgur983 <rkddlsgur983@naver.com>
2019-09-27 16:06:00 +0900

    feat: Add trip dialog value deliver to main

--- app/src/main/java/com/soksok/seoulmate/view/main/ChangeAlbumTitleDialog.java
+++ app/src/main/java/com/soksok/seoulmate/view/main/ChangeAlbumTitleDialog.java
@@ -2,6 +2,7 @@ package com.soksok.seoulmate.view.main;
 
 import androidx.annotation.NonNull;
 import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.MutableLiveData;
 
 import android.app.Dialog;
 import android.content.Context;
@@ -9,11 +10,15 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.Window;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.soksok.seoulmate.R;
 
 public class ChangeAlbumTitleDialog extends Dialog {
+
+    public MutableLiveData<String> albumTitle = new MutableLiveData<>();
 
     public ChangeAlbumTitleDialog(@NonNull Context context) {
         super(context);
@@ -29,5 +34,19 @@ public class ChangeAlbumTitleDialog extends Dialog {
 
         ImageView closeBtn = findViewById(R.id.iv_close);
         closeBtn.setOnClickListener(v -> dismiss());
+
+        EditText edTitle = findViewById(R.id.ed_album_title);
+
+        TextView tvOk = findViewById(R.id.tv_apply_ok);
+        tvOk.setOnClickListener(v -> {
+            /** 텍스트 있는 경우만
+             *  앨범 제목 변경
+             */
+            String title = edTitle.getText().toString();
+            if (!title.equals("")) {
+                albumTitle.postValue(title);
+            }
+            dismiss();
+        });
     }
 }
